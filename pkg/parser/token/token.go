cc7f012808adc3d92e32ceca3f9adf56fe781b99
Flier Lu <flier.lu@gmail.com>
2022-03-16 19:31:04 +0800

    add more token parser

--- pkg/parser/token/token.go
+++ pkg/parser/token/token.go
@@ -9,16 +9,33 @@ import (
 )
 
 type UnexpectedErr[T stream.Token] struct {
-	Expected T
-	Actual   T
+	Expected []T
+	Actual   []T
 }
 
 func (e *UnexpectedErr[T]) Error() string {
-	return fmt.Sprintf("expected `%c`, got `%c`", e.Expected, e.Actual)
+	if e.Expected != nil {
+		return fmt.Sprintf("expected `%c`, got `%c`", e.Expected, e.Actual)
+	}
+
+	return fmt.Sprintf("unexpected `%c`", e.Actual)
 }
 
-func Unexpected[T stream.Token](expected, actual T) error {
+func Unexpected[T stream.Token](expected, actual []T) error {
 	return &UnexpectedErr[T]{expected, actual}
+}
+
+// Any parses any token.
+func Any[S stream.Stream[T], T stream.Token, P parser.ParseFunc[S, T, T]]() P {
+	return func(input S) (actual T, remaining S, err error) {
+		if len(input) == 0 {
+			err = io.ErrUnexpectedEOF
+		} else {
+			actual, remaining = input[0], input[1:]
+		}
+
+		return
+	}
 }
 
 // Token parses a character and succeeds if the character is equal to `tok`.
@@ -31,13 +48,18 @@ func Token[S stream.Stream[T], T stream.Token, P parser.ParseFunc[S, T, T]](tok 
 		} else {
 			actual, remaining = input[0], input
 
-			err = Unexpected(tok, actual)
+			err = Unexpected([]T{tok}, []T{actual})
 		}
 		return
 	}
 }
 
-func Tokens[S stream.Stream[T], T stream.Token, P parser.ParseFunc[S, T, []T]](cmp func(lhs, rhs T) bool, tokens S) P {
+// Tokens parses multiple tokens.
+//
+// Consumes items from the input and compares them to the values from `tokens` using the
+/// comparison function `cmp`. Succeeds if all the items from `tokens` are matched in the input
+/// stream and fails otherwise with `expected` used as part of the error.
+func Tokens[S stream.Stream[T], T stream.Token, P parser.ParseFunc[S, T, []T]](cmp func(lhs, rhs T) bool, expected, tokens S) P {
 	return func(input S) (actual []T, remaining S, err error) {
 		n := len(tokens)
 		if len(input) < n {
@@ -45,7 +67,7 @@ func Tokens[S stream.Stream[T], T stream.Token, P parser.ParseFunc[S, T, []T]](c
 		} else {
 			for i, tok := range tokens {
 				if !cmp(input[i], tok) {
-					err = Unexpected(tok, input[i])
+					err = Unexpected(expected, input[:n])
 					return
 				}
 			}
@@ -53,5 +75,71 @@ func Tokens[S stream.Stream[T], T stream.Token, P parser.ParseFunc[S, T, []T]](c
 			actual, remaining = input[:n], input[n:]
 		}
 		return
+	}
+}
+
+// OneOf extract one token and succeeds if it is part of `tokens`.
+func OneOf[S stream.Stream[T], T stream.Token, P parser.ParseFunc[S, T, T]](tokens S) P {
+	return func(input S) (actual T, remaining S, err error) {
+		if len(input) == 0 {
+			err = io.ErrUnexpectedEOF
+		} else {
+			for _, tok := range tokens {
+				if input[0] == tok {
+					actual, remaining = input[0], input[1:]
+					return
+				}
+			}
+
+			actual, remaining = input[0], input
+
+			err = Unexpected(tokens, input[:1])
+		}
+		return
+	}
+}
+
+// NoneOf extract one token and succeeds if it is not part of `tokens`.
+func NoneOf[S stream.Stream[T], T stream.Token, P parser.ParseFunc[S, T, T]](tokens S) P {
+	return func(input S) (actual T, remaining S, err error) {
+		if len(input) == 0 {
+			err = io.ErrUnexpectedEOF
+		} else {
+			for _, tok := range tokens {
+				if input[0] == tok {
+					actual, remaining = input[0], input
+					err = Unexpected(nil, input[:1])
+					return
+				}
+			}
+
+			actual, remaining = input[0], input[1:]
+		}
+		return
+	}
+}
+
+// Value always returns the value `v` without consuming any input.
+func Value[S stream.Stream[T], T stream.Token, P parser.ParseFunc[S, T, T]](v T) P {
+	return func(input S) (T, S, error) {
+		return v, input, nil
+	}
+}
+
+// Produce always returns the value produced by calling `f`.
+func Produce[S stream.Stream[T], T stream.Token, O any, P parser.ParseFunc[S, T, O]](f func() O) P {
+	return func(input S) (O, S, error) {
+		return f(), input, nil
+	}
+}
+
+// Eof succeeds only if the stream is at end of input, fails otherwise.
+func Eof[S stream.Stream[T], T stream.Token, P parser.ParseFunc[S, T, bool]]() P {
+	return func(input S) (bool, S, error) {
+		if len(input) == 0 {
+			return true, input, nil
+		}
+
+		return false, input, Unexpected(nil, input)
 	}
 }
--- pkg/parser/token/token_test.go
+++ pkg/parser/token/token_test.go
@@ -1,6 +1,7 @@
 package token_test
 
 import (
+	"fmt"
 	"io"
 	"testing"
 	"unicode"
@@ -25,7 +26,7 @@ func TestToken(t *testing.T) {
 				Convey("Then parse again should get an error", func() {
 					c, s, err = p([]rune(s))
 
-					So(err, ShouldBeError, token.Unexpected('h', 'e'))
+					So(err, ShouldBeError, token.Unexpected([]rune{'h'}, []rune{'e'}))
 					So(string(s), ShouldEqual, "ello")
 				})
 			})
@@ -41,7 +42,7 @@ func TestToken(t *testing.T) {
 
 func TestTokens(t *testing.T) {
 	Convey("Given a tokens parser", t, func() {
-		p := token.Tokens(func(lhs, rhs byte) bool { return lhs == rhs }, []byte("foo"))
+		p := token.Tokens(func(lhs, rhs byte) bool { return lhs == rhs }, []byte("foo"), []byte("foo"))
 
 		Convey("When parse string", func() {
 			a, s, err := p([]byte("foobar"))
@@ -55,7 +56,7 @@ func TestTokens(t *testing.T) {
 			Convey("Then parse again should get an error", func() {
 				_, _, err = p(s)
 
-				So(err, ShouldBeError, token.Unexpected('f', 'b'))
+				So(err, ShouldBeError, token.Unexpected([]byte("foo"), []byte("bar")))
 			})
 
 			Convey("When I parse a empty string", func() {
@@ -65,7 +66,7 @@ func TestTokens(t *testing.T) {
 			})
 		})
 
-		p2 := token.Tokens(func(lhs, rhs rune) bool { return unicode.ToLower(lhs) == unicode.ToLower(rhs) }, []rune("foo"))
+		p2 := token.Tokens(func(lhs, rhs rune) bool { return unicode.ToLower(lhs) == unicode.ToLower(rhs) }, []rune("[fF][oO][oO]"), []rune("foo"))
 
 		Convey("When parse case insensitive string ", func() {
 			a, s, err := p2([]rune("foobar"))
@@ -77,4 +78,88 @@ func TestTokens(t *testing.T) {
 			})
 		})
 	})
+}
+
+func ExampleAny() {
+	p := token.Any[[]rune]()
+
+	fmt.Println(p([]rune("apple")))
+	fmt.Println(p(nil))
+
+	// Output:
+	// 97 [112 112 108 101] <nil>
+	// 0 [] unexpected EOF
+}
+
+func ExampleToken() {
+	p := token.Token[[]rune]('a')
+
+	fmt.Println(p([]rune("apple")))
+	fmt.Println(p([]rune("foobar")))
+
+	// Output:
+	// 97 [112 112 108 101] <nil>
+	// 102 [102 111 111 98 97 114] expected `[a]`, got `[f]`
+}
+
+func ExampleTokens() {
+	p := token.Tokens(func(l, r rune) bool { return unicode.ToLower(l) == unicode.ToLower(r) }, []rune("[fF][oO][oO]"), []rune("foo"))
+
+	fmt.Println(p([]rune("apple")))
+	fmt.Println(p([]rune("FooBar")))
+
+	// Output:
+	// [] [] expected `[[ f F ] [ o O ] [ o O ]]`, got `[a p p]`
+	// [70 111 111] [66 97 114] <nil>
+}
+
+func ExampleOneOf() {
+	p := token.OneOf([]rune("abc"))
+
+	fmt.Println(p([]rune("apple")))
+	fmt.Println(p([]rune("foobar")))
+
+	// Output:
+	// 97 [112 112 108 101] <nil>
+	// 102 [102 111 111 98 97 114] expected `[a b c]`, got `[f]`
+}
+
+func ExampleNoneOf() {
+	p := token.NoneOf([]rune("abc"))
+
+	fmt.Println(p([]rune("apple")))
+	fmt.Println(p([]rune("foobar")))
+
+	// Output:
+	// 97 [97 112 112 108 101] unexpected `[a]`
+	// 102 [111 111 98 97 114] <nil>
+}
+
+func ExampleValue() {
+	p := token.Value[[]rune]('a')
+
+	fmt.Println(p([]rune("pple")))
+
+	// Output:
+	// 97 [112 112 108 101] <nil>
+}
+
+func ExampleProduce() {
+	p := token.Produce[[]rune](func() string { return "foo" })
+
+	fmt.Println(p([]rune("bar")))
+
+	// Output:
+	// foo [98 97 114] <nil>
+}
+
+func ExampleEof() {
+	p := token.Eof[[]rune]()
+
+	fmt.Println(p(nil))
+	fmt.Println(p([]rune("foobar")))
+
+	// Output:
+	// true [] <nil>
+	// false [102 111 111 98 97 114] unexpected `[f o o b a r]`
 }

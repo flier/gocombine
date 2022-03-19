5b8a6b522530bdb6c9112ea263fe52449f9920e8
Flier Lu <flier.lu@gmail.com>
2022-03-19 18:04:33 +0800

    add ranges.LengthPrefix

---
+++ pkg/parser/ranges/length_prefix.go
@@ -0,0 +1,26 @@
+package ranges
+
+import (
+	"github.com/flier/gocombine/pkg/parser"
+	"github.com/flier/gocombine/pkg/stream"
+	"golang.org/x/exp/constraints"
+)
+
+// LengthPrefix takes a parser which parses a `length` then extracts a range of that length and returns it.
+// Commonly used in binary formats
+func LengthPrefix[
+	S stream.Stream[T],
+	T stream.Token,
+	N constraints.Integer,
+](length parser.Func[S, T, N]) parser.Func[S, T, []T] {
+	return func(input S) (out []T, remaining S, err error) {
+		var n N
+		n, remaining, err = length(input)
+		if err != nil {
+			return
+		}
+
+		out, remaining, err = stream.UnconsRange(remaining, int(n))
+		return
+	}
+}
---
+++ pkg/parser/ranges/length_prefix_test.go
@@ -0,0 +1,17 @@
+package ranges_test
+
+import (
+	"fmt"
+
+	"github.com/flier/gocombine/pkg/parser/bytes/be"
+	"github.com/flier/gocombine/pkg/parser/ranges"
+)
+
+func ExampleLengthPrefix() {
+	p := ranges.LengthPrefix(be.Uint16[[]byte]())
+
+	fmt.Println(p([]byte{0x00, 0x03, 0x01, 0x02, 0x03, 0x04}))
+
+	// Output:
+	// [1 2 3] [4] <nil>
+}

ea4af584ceb1f9dbac956173809ed1617876efc5
Flier Lu <flier.lu@gmail.com>
2022-03-19 21:13:22 +0800

    add repeat.Escaped

---
+++ pkg/parser/repeat/escaped.go
@@ -0,0 +1,53 @@
+package repeat
+
+import (
+	"github.com/flier/gocombine/pkg/parser"
+	"github.com/flier/gocombine/pkg/stream"
+)
+
+// Escaped parses an escaped string by first applying `parser`
+// which accept the normal characters which do not need escaping.
+// Once `parser` can not consume any more input it checks if the next token is `escape`.
+// If it is then `escape_parser` is used to parse the escaped character and then resumes parsing using `parser`.
+// If `escape` was not found then the parser finishes successfully.
+func Escaped[
+	S stream.Stream[T],
+	T stream.Token,
+](
+	parser parser.Func[S, T, []T],
+	escape T,
+	escape_parser parser.Func[S, T, T],
+) parser.Func[S, T, []T] {
+	return func(input S) (parsed []T, remaining S, err error) {
+		remaining = input
+
+		for {
+			var outs []T
+			var rest S
+			if outs, rest, err = parser(remaining); err != nil {
+				var tok T
+
+				if tok, rest, err = stream.Uncons(remaining); err != nil {
+					return
+				}
+
+				if tok != escape {
+					break
+				}
+
+				outs = []T{tok}
+
+				if tok, rest, err = escape_parser(rest); err != nil {
+					return
+				}
+
+				outs = append(outs, tok)
+			}
+
+			parsed = append(parsed, outs...)
+			remaining = rest
+		}
+
+		return
+	}
+}
---
+++ pkg/parser/repeat/escaped_test.go
@@ -0,0 +1,23 @@
+package repeat_test
+
+import (
+	"fmt"
+
+	"github.com/flier/gocombine/pkg/parser/char"
+	"github.com/flier/gocombine/pkg/parser/ranges"
+	"github.com/flier/gocombine/pkg/parser/repeat"
+	"github.com/flier/gocombine/pkg/parser/token"
+)
+
+func ExampleEscaped() {
+	p := char.AsString(repeat.Escaped(
+		ranges.TakeWhile1[[]rune](func(c rune) bool { return c != '"' && c != '\\' }),
+		'\\',
+		token.OneOf([]rune(`nrt\"`)),
+	))
+
+	fmt.Println(p([]rune(`ab\"12\n\rc"`)))
+
+	// Output:
+	// ab\"12\n\rc [34] <nil>
+}

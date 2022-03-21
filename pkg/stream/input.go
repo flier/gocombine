08c49653d6a4f93cd93345faf1cb4f12cd2fe1e0
Flier Lu <flier.lu@gmail.com>
2022-03-21 14:54:48 +0800

    add byte and rune

--- pkg/stream/input.go
+++ pkg/stream/input.go
@@ -2,12 +2,18 @@ package stream
 
 // Input represents a stream-like input.
 type Input interface {
-	~[]byte | ~[]uint16 | ~[]rune | ~string
+	byte | rune | ~[]byte | ~[]uint16 | ~[]rune | ~string
 }
 
 // New returns a stream from a slice of tokens.
 func New[S Stream[T], T Token, I Input](input I) (s S) {
 	switch v := interface{}(input).(type) {
+	case byte:
+		s = New[S]([]byte{v})
+
+	case rune:
+		s = New[S]([]rune{v})
+
 	case []byte:
 		s, _ = interface{}(v).([]T)
 
@@ -18,7 +24,7 @@ func New[S Stream[T], T Token, I Input](input I) (s S) {
 		s, _ = interface{}(v).([]T)
 
 	case string:
-		s, _ = interface{}([]rune(v)).([]T)
+		s = New[S]([]rune(v))
 	}
 
 	return
